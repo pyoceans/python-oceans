c08134ce2b0f29c3a4239f18d7640cf974675177
Filipe Fernandes <ocefpaf@gmail.com>
2012-02-12 14:04:00 -0500

    Moved some functions to the new oceans/ff_tools/time_series.py

--- oceans/ff_tools/ff_tools.py
+++ oceans/ff_tools/ff_tools.py
@@ -1,101 +1,21 @@
+# -*- coding: utf-8 -*-
+#
+# ff_tools.py
+#
+# purpose:
+# author:   Filipe P. A. Fernandes
+# e-mail:   ocefpaf@gmail
+# web:      http://ocefpaf.tiddlyspot.com/
+# created:  12-Feb-2012
+# modified:
+#
+# obs:
+#
+
 from __future__ import division
 
 import numpy as np
 import numpy.ma as ma
-
-
-def smoo1(datain, window_len=11, window='hanning'):
-    r"""
-    Smooth the data using a window with requested size.
-
-    Parameters
-    ----------
-    datain : array_like
-             input series
-    window_len : int
-                 size of the smoothing window; should be an odd integer
-    window : str
-             window from 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'.
-             flat window will produce a moving average smoothing.
-
-    Returns
-    -------
-    data_out : array_like
-            smoothed signal
-
-    See Also
-    --------
-    binave, binavg
-    np.hanning, np.hamming, np.bartlett, np.blackman, np.convolve,
-    scipy.signal.lfilter
-
-    Notes
-    -----
-    original from: http://www.scipy.org/Cookbook/SignalSmooth
-    This method is based on the convolution of a scaled window with the signal.
-    The signal is prepared by introducing reflected copies of the signal (with
-    the window size) in both ends so that transient parts are minimized in the
-    beginning and end part of the output signal.
-
-    Examples
-    --------
-    >>> import matplotlib.pyplot as plt
-    >>> import numpy as np
-    >>> import ff_tools as ff
-    >>> time = np.linspace( -4, 4, 100 )
-    >>> series = np.sin(time)
-    >>> noise_series = series + np.random.randn( len(time) ) * 0.1
-    >>> data_out = ff.smoo1(series)
-    >>> ws = 31
-    >>> plt.subplot(211)
-    >>> plt.plot( np.ones(ws) )
-    >>> windows = ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']
-    >>> plt.hold(True)
-    >>> for w in windows[1:]:
-    >>>     eval('plt.plot(np.'+w+'(ws) )')
-    >>> plt.axis([0,30,0,1.1])
-    >>> plt.legend(windows)
-    >>> plt.title("The smoothing windows")
-    >>> plt.subplot(212)
-    >>> plt.plot(series)
-    >>> plt.plot(noise_series)
-    >>> for w in windows:
-    >>>     plt.plot( ff.smoo1(noise_series, 10, w) )
-    >>> l = ['original signal', 'signal with noise']
-    >>> l.extend(windows)
-    >>> plt.legend(l)
-    >>> plt.title("Smoothing a noisy signal")
-    >>> plt.show()
-
-    TODO: window parameter can be the window itself (i.e. an array)
-    instead of a string.
-    """
-
-    datain = np.asarray(datain)
-
-    if datain.ndim != 1:
-        raise ValueError("smooth only accepts 1 dimension arrays.")
-
-    if datain.size < window_len:
-        raise ValueError("Input vector needs to be bigger than window size.")
-
-    if window_len < 3:
-        return datain
-
-    if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
-        raise ValueError("""Window is on of 'flat', 'hanning', 'hamming',
-                         'bartlett', 'blackman'""")
-
-    s = np.r_[2 * datain[0] - datain[window_len:1:-1], datain, 2 *
-                                    datain[-1] - datain[-1:-window_len:-1]]
-
-    if window == 'flat':  # Moving average.
-        w = np.ones(window_len, 'd')
-    else:
-        w = eval('np.' + window + '(window_len)')
-
-    data_out = np.convolve(w / w.sum(), s, mode='same')
-    return data_out[window_len - 1:-window_len + 1]
 
 
 def sub2ind(shape, I, J, row_major=True):
