dd913e92ef623b68ab3f246b7593671ee671d22d
Filipe Fernandes <ocefpaf@gmail.com>
2012-05-25 16:48:02 -0400

    Eliminated `sub2ind()` Moved `del_eta_del_x()` to ocfis.py Added a __all__ list. Most functions have docstring and doctest. Some minor fixes

--- oceans/ff_tools/ff_tools.py
+++ oceans/ff_tools/ff_tools.py
@@ -7,65 +7,32 @@
 # e-mail:   ocefpaf@gmail
 # web:      http://ocefpaf.tiddlyspot.com/
 # created:  12-Feb-2012
-# modified: Thu 03 May 2012 10:04:04 AM EDT
+# modified: Fri 25 May 2012 03:59:47 PM EDT
 #
 # obs:
 #
 
 from __future__ import division
 
+import warnings
+
 import numpy as np
 import numpy.ma as ma
+from scipy.ndimage import map_coordinates
 
+from dateutil import rrule, parser
 
-def sub2ind(shape, I, J, row_major=True):
-    r"""
-    Quick-and-dirty matlab sub2ind substitute
-    TODO: enter integers-math-with-float-output-integer
-    """
-    if row_major:
-        ind = (I % shape[0]) * shape[1] + (J % shape[1])
-    else:
-        ind = (J % shape[1]) * shape[0] + (I % shape[0])
-
-    ind = np.int64(ind)
-
-    #inds = sub2ind(shape, z, y, x)
-    # or
-    #inds = z + y * shape[0] + x * shape[0] * shape[1]
-
-    return ind
-
-
-def del_eta_del_x(U, f, g, balance, R=None):
-    r"""
-    Calculate :mat: `\frac{\partial \eta} {\partial x}`
-    for different force balances
-
-    Parameters:
-    ----------
-    U : array_like
-        velocity magnitude [m/s]
-    balance : str
-              geostrophic, gradient or max_gradient
-    """
-
-    if balance == 'geostrophic':
-        detadx = f * U / g
-
-    elif balance == 'gradient':
-        detadx = (U ** 2 / R + f * U) / g
-
-    elif balance == 'max_gradient':
-        detadx = (R * f ** 2) / (4 * g)
-
-    return detadx
+__all__ = [
+           'get_profile',
+           'gen_dates',
+           'princomp',
+           'strip_mask',
+           'shiftdim'
+           ]
 
 
 def get_profile(x, y, f, xi, yi, order=3):
-    import scipy.ndimage.map_coordinates
-    r"""
-    Interpolate regular data.
+    r"""Interpolate regular data.
 
     Parameters
     ----------
@@ -97,14 +64,32 @@ def get_profile(x, y, f, xi, yi, order=3):
         the value of the interpolating spline at :math:`xi,yi`
 
 
-    """
-    conditions = [xi.min() < x.min(),
-                  xi.max() > x.max(),
-                  yi.min() < y.min(),
-                  yi.max() > y.max()]
+    Examples
+    --------
+    >>> import numpy as np
+    >>> import oceans.ff_tools as ff
+    >>> x, y = np.meshgrid(range(360), range(91))
+    >>> f = np.array(range(91 * 360)).reshape((91, 360))
+    >>> Paris = [2.4, 48.9]
+    >>> Rome = [12.5, 41.9]
+    >>> Greenwich = [0, 51.5]
+    >>> xi = Paris[0], Rome[0], Greenwich[0]
+    >>> yi = Paris[1], Rome[1], Greenwich[1]
+    >>> ff.get_profile(x, y, f, xi, yi, order=3)
 
-    if True in conditions:
-        print "Warning, extrapolation in being done!!"
+    Notes
+    -----
+    http://mail.scipy.org/pipermail/scipy-user/2011-June/029857.html
+    """
+
+    x, y, f, xi, yi = map(np.asanyarray, (x, y, f, xi, yi))
+    conditions = np.array([xi.min() < x.min(),
+                           xi.max() > x.max(),
+                           yi.min() < y.min(),
+                           yi.max() > y.max()])
+
+    if conditions.any():
+        warnings.warn("Warning, extrapolation in being done!!")
 
     dx = x[0, 1] - x[0, 0]
     dy = y[1, 0] - y[0, 0]
@@ -114,30 +99,28 @@ def get_profile(x, y, f, xi, yi, order=3):
 
     coords = np.array([ivals, jvals])
 
-    return  scipy.ndimage.map_coordinates(f, coords, mode='nearest',
-                                                        order=order)
+    return  map_coordinates(f, coords, mode='nearest', order=order)
 
 
 def gen_dates(start, end, dt=None):
-    r"""
-    start = '1980-01-19'
-    end = datetime.datetime.utcnow().strftime("%Y-%m-%d")
+    r"""Date range from `start` to `end` at `dt` intervals.
+
+    Examples
+    --------
+    >>> import datetime
+    >>> from dateutil import rrule
+    >>> start = '1980-01-19'
+    >>> end = datetime.datetime.utcnow().strftime("%Y-%m-%d")
     list(gen_dates(start, end, dt=rrule.YEARLY))
     """
-    from dateutil import rrule, parser
-    dates = (rrule.rrule(dt,
-                         dtstart=parser.parse(start),
-                         until=parser.parse(end)))
+    dates = (rrule.rrule(dt, dtstart=parser.parse(start),
+                             until=parser.parse(end)))
     return dates
 
 
 def princomp(A, numpc=None):
-    r"""
-    Performs principal components analysis (PCA) on the n-by-p data matrix A
-    Rows of A correspond to observations, columns to variables.
-
-    http://glowingpython.blogspot.com/
-    2011/07/principal-component-analysis-with-numpy.html?spref=tw
+    r"""Performs principal components analysis (PCA) on the n-by-p data matrix
+    `A`.  Rows of A correspond to observations, columns to variables.
 
     Returns :
         coeff :
@@ -153,57 +136,57 @@ def princomp(A, numpc=None):
 
     Examples
     --------
-    # 2D dataset.
+    >>> import numpy as np
+    >>> import matplotlib.pyplot as plt
+    >>> import oceans.ff_tools as ff
+    >>> # 2D dataset.
     >>> A = np.array([ [2.4,0.7,2.9,2.2,3.0,2.7,1.6,1.1,1.6,0.9],
                      [2.5,0.5,2.2,1.9,3.1,2.3,2,1,1.5,1.1] ])
-    >>> coeff, score, latent = princomp(A.T)
-    >>> plt.figure()
-    >>> plt.subplot(121)
-    # every eigenvector describe the direction
-    # of a principal component.
+    >>> coeff, score, latent = ff.princomp(A.T)
+    >>> fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2)
+    >>> # Every eigenvector describe the direction
+    >>> # of a principal component.
     >>> m = np.mean(A, axis=1)
-    >>> plt.plot([0, -coeff[0,0] * 2] + m[0], [0, -coeff[0,1] * 2] +
+    >>> ax1.plot([0, -coeff[0,0] * 2] + m[0], [0, -coeff[0,1] * 2] +
     ...                                                           m[1], '--k')
-    >>> plt.plot([0, coeff[1,0] * 2] + m[0], [0, coeff[1,1] * 2] +
+    >>> ax1.plot([0, coeff[1,0] * 2] + m[0], [0, coeff[1,1] * 2] +
     ...                                                           m[1], '--k')
-    >>> plt.plot(A[0,:], A[1,:], 'ob')  # The data.
-    >>> plt.axis('equal')
-    >>> plt.subplot(122)
-    # New data.
-    >>> plt.plot(score[0,:], score[1,:], '*g')
-    >>> plt.axis('equal')
+    >>> ax1.plot(A[0,:], A[1,:], 'ob')  # The data.
+    >>> ax1.axis('equal')
+    >>> # New data.
+    >>> ax2.plot(score[0,:], score[1,:], '*g')
+    >>> ax2.axis('equal')
     >>> plt.show()
-
-    # 4D dataset.
+    >>> # 4D dataset.
     >>> A = np.array([[-1, 1, 2, 2],
-                      [-2, 3, 1, 0],
-                      [ 4, 0, 3,-1]], dtype=np.double)
-    >>> coeff, score, latent = princomp(A)
+    ...               [-2, 3, 1, 0],
+    ...               [ 4, 0, 3,-1]], dtype=np.double)
+    >>> coeff, score, latent = ff.princomp(A)
     >>> perc = np.cumsum(latent) / np.sum(latent)
-    >>> plt.figure()
-    # The following plot show that first two components
-    # account for 100% of the variance.
-    >>> plt.stem(range(len(perc)), perc, '--b')
-    >>> plt.axis([-0.3,4.3,0,1.3])
+    >>> fig, ax = plt.subplots()
+    >>> # The following plot show that first two components account for
+    >>> # 100% of the variance.
+    >>> ax.stem(range(len(perc)), perc, '--b')
+    >>> ax.axis([-0.3, 4.3, 0, 1.3])
     >>> plt.show()
-    >>> print('the principal component scores')
-    >>> print(score.T)  # Only the first two columns are nonzero.
-    >>> print('The rank of A is')
-    >>> print(np.rank(A))  # Indeed, the rank of A is 2.
-
-    # Image example:
-    >>> A = plt.imread('image.jpg') # load an image
+    >>> # Image example:
+    >>> import matplotlib.cbook as cbook
+    >>> from matplotlib.ticker import NullLocator
+    >>> img = cbook.get_sample_data('lena.jpg', asfileobj=False)
+    >>> A = plt.imread(img) # load an image
     >>> A = np.mean(A, 2)  # to get a 2-D array
     >>> full_pc = np.size(A, axis=1)  # numbers of all the principal components
     >>> i, dist = 1, []
-    >>> for numpc in range(0, full_pc+10, 10):  # 0 10 20 ... full_pc
-    >>>     coeff, score, latent = princomp(A, numpc)
-    >>>     Ar = np.dot(coeff, score).T + np.mean(A, axis=0)  # Reconstruction.
-            # difference in Frobenius norm
-    >>>     dist.append(linalg.norm(A - Ar, 'fro'))
-            # showing the pics reconstructed with less than 50 PCs
+    >>> for numpc in range(0, full_pc+20, 20):
+    >>>     coeff, score, latent = ff.princomp(A, numpc)
+    >>>     # Reconstruction.difference in Frobenius norm
+    >>>     Ar = np.dot(coeff, score).T + np.mean(A, axis=0)
+    >>>     dist.append(np.linalg.norm(A - Ar, 'fro'))
+    >>>     # Showing the pics reconstructed with less than 50 PCs
     >>>     if numpc <= 50:
-    >>>         ax = plt.subplot(2, 3, i, frame_on=False)
+    >>>         i += 1
+    >>>         print(i)
+    >>>         ax = plt.subplot(2, 4, i, frame_on=False)
     >>>         ax.xaxis.set_major_locator(NullLocator())  # remove ticks
     >>>         ax.yaxis.set_major_locator(NullLocator())
     >>>         i += 1
@@ -228,6 +211,11 @@ def princomp(A, numpc=None):
     >>>          range(0, full_pc + 10, 10), dist, 'r')
     >>> plt.axis([0, full_pc, 0, 1.1])
     >>> plt.show()
+
+    Notes
+    -----
+    http://glowingpython.blogspot.com/
+    2011/07/principal-component-analysis-with-numpy.html?spref=tw
     """
 
     # computing eigenvalues and eigenvectors of covariance matrix
@@ -250,10 +238,7 @@ def princomp(A, numpc=None):
 
 
 def strip_mask(arr, fill_value=np.NaN):
-    r"""
-    Take a masked array and return its data(filled) + mask
-
-    """
+    r"""Take a masked array and return its data(filled) + mask."""
     if ma.isMaskedArray(arr):
         mask = np.ma.getmaskarray(arr)
         arr = np.ma.filled(arr, fill_value)
@@ -263,23 +248,36 @@ def strip_mask(arr, fill_value=np.NaN):
 
 
 def shiftdim(x, n=None):
+    r""" Matlab's shiftdim in python.
+
+    Examples
+    --------
+    >>> import oceans.ff_tools as ff
+    >>> a = np.random.rand(1,1,3,1,2)
+    >>> print("a shape and dimension: %s, %s" % (a.shape, a.ndim))
+    a shape and dimension: (1, 1, 3, 1, 2), 5
+    >>> # print(range(a.ndim))
+    >>> # print(np.roll(range(a.ndim), -2))
+    >>> # print(a.transpose(np.roll(range(a.ndim), -2)))
+    >>> b = ff.shiftdim(a)
+    >>> print("b shape and dimension: %s, %s" % (b.shape, b.ndim))
+    b shape and dimension: (3, 1, 2), 3
+    >>> c = ff.shiftdim(b, -2)
+    >>> c.shape == a.shape
+    True
+
+    Notes
+    -----
+    http://www.python-it.org/forum/index.php?topic=4688.0
     """
-    #http://www.python-it.org/forum/index.php?topic=4688.0
-    a = np.rand(1,1,3,1,2)
-    print(a.shape)
-    print(a.ndim)
-    print(range(a.ndim))
-    print(np.roll(range(a.ndim), -2))
-    print(a.transpose(np.roll(range(a.ndim), -2)))
 
-    b = shiftdim(a)
-    print(b.shape)
-
-    c = shiftdim(b, -2)
-    print(c.shape)
-
-    print(c==a)
-    """
+    def no_leading_ones(shape):
+        shape = np.atleast_1d(shape)
+        if shape[0] == 1:
+            shape = shape[1:]
+            return no_leading_ones(shape)
+        else:
+            return shape
 
     if n is None:
         # returns the array B with the same number of
@@ -295,12 +293,6 @@ def shiftdim(x, n=None):
         # to the right and pads with singletons.
         return x.reshape((1,) * -n + x.shape)
 
-
-def no_leading_ones(x):
-    """Used in shiftdim."""
-    x = np.atleast_1d(x)
-    if x[0] == 1:
-        x = x[1:]
-        return no_leading_ones(x)
-    else:
-        return x
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
