cae5e4963f82e75da4e0b7bf8e6de837cb328edd
Filipe Fernandes <ocefpaf@gmail.com>
2012-06-26 16:23:49 -0300

    Added a Sandbox `module` with some work-in-progress material.

---
+++ oceans/sandbox/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#
+# __init__.py
+#
+# purpose:  Random work-in-progress stuff.
+# author:   Filipe P. A. Fernandes
+# e-mail:   ocefpaf@gmail
+# web:      http://ocefpaf.tiddlyspot.com/
+# created:  22-Jun-2011
+# modified: Tue 26 Jun 2012 04:22:15 PM BRT
+#
+# obs:
+#
+
+from sandbox import *
