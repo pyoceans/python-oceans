1a7ab74a2c7a20fd7fa655321e5fc4e4b583e2e5
Filipe Fernandes <ocefpaf@gmail.com>
2013-02-21 17:29:34 -0300

    Changed: `despike` to mimic similar SeaBird "WildEdit."          `from_cnv` returns a tuple with downcast and upcast from split.          Major overhaul in the examples.          TODO: Make these examples into tests/docs. Added: `press_check` method to eliminate pressure inversions.        `seabird_filter` method a "Filter-like" procedure.        `movingaverage` function.        `swap_index` easy way to change from `pressure` to `scan` as index (or        any other data (column).

--- oceans/ctd/ctd.py
+++ oceans/ctd/ctd.py
@@ -7,7 +7,7 @@
 # e-mail:   ocefpaf@gmail
 # web:      http://ocefpaf.tiddlyspot.com/
 # created:  22-Jun-2012
-# modified: Fri 15 Feb 2013 03:16:43 PM BRST
+# modified: Thu 21 Feb 2013 05:22:27 PM BRT
 #
 # obs: Instead of sub-classing I opted for a "Monkey Patch" approach
 #      (Wes suggestion).
@@ -40,6 +40,7 @@ import numpy.ma as ma
 import matplotlib.pyplot as plt
 import mpl_toolkits.axisartist as AA
 
+from scipy import signal
 from scipy.interpolate import interp1d
 from scipy.stats import nanmean, nanstd
 from pandas import Index, Series, DataFrame
@@ -65,6 +66,11 @@ def find_outlier(point, window, n):
     return np.abs(point - nanmean(window)) >= n * nanstd(window)
 
 
+def movingaverage(series, window_size=48):
+    window = np.ones(int(window_size)) / float(window_size)
+    return Series(np.convolve(series, window, 'same'), index=series.index)
+
+
 def asof(self, label):
     if label not in self:
         loc = self.searchsorted(label, side='left')
@@ -86,6 +92,50 @@ def check_index(ix0, ix1):
             return ix0, ix1
     else:
         raise ValueError("Series index must be the same.")
+
+
+def split(self):
+    r"""Returns a tupple with down- and up-cast."""
+    down = self.ix[:self.index.argmax()]
+    up = self.ix[self.index.argmax():]
+    return down, up
+
+
+def press_check(self):
+    r"""Remove pressure reversal. NOTE: Downcast only."""
+    data = self.copy()
+    press = data.index.values
+    ref = press[0]
+    inversions = np.diff(np.r_[press, press[-1]]) < 0
+    mask = np.zeros_like(inversions)
+    for k, p in enumerate(inversions):
+        if p:
+            ref = press[k]
+            cut = press[k + 1:] < ref
+            mask[k + 1:][cut] = True
+    data[mask] = np.NaN
+    return data
+
+
+def seabird_filter(self, sample_rate=24.0, time_constat=0.15):
+    r"""Filter a series with `time_constat` (use 0.15 s for pressure), and
+    for a signal of `sample_rate` in Hertz (24 Hz if 911CTD).
+    NOTE: Seabird actually uses a cosine filter.  I'm use a kaiser filter.
+    NOTE: 911 (9+) does note require filter for temperature nor salinity.
+    """
+
+    series = self.index.values  # Pressure
+    nyq_rate = sample_rate / 2.0
+    width = 5.0 / nyq_rate  # 5 Hz transition rate.
+    ripple_db = 60.0  # Attenuation at the stop band.
+    N, beta = signal.kaiserord(ripple_db, width)
+
+    cutoff_hz = (1. / time_constat)  # Cutoff frequency at 0.15 s.
+    taps = signal.firwin(N, cutoff_hz / nyq_rate, window=('kaiser', beta))
+    filtered = signal.filtfilt(taps, [1.0], series)
+
+    self['filtered_press'] = filtered
+    return self.swap_index('filtered_press')
 
 
 def get_maxdepth(self):
@@ -332,25 +382,41 @@ def smooth(self, window_len=11, window='hanning'):
     return Series(data, index=self.index, name=self.name)
 
 
-def despike(self, n=3, windowsize=10, verbose=False):
-    r"""Replace spikes with np.NaN.  Removing spikes that are >= n * std.
-    default n = 3."""
+def despike(self, std1=20, std2=2, block=100, keep=0):
+    r"""Wild Edit Seabird-like function.  NOTE: Must before bindata!
+    Two passes with Standard deviation criteria `std1` and `std2` at scan
+    `block`.  TODO: keep data within distance of the mean."""
 
+    # First run with std1:
     data = self.values.copy()
     for k, point in enumerate(data):
-        if k <= windowsize:
-            window = data[k:k + windowsize]
-            if find_outlier(point, window, n):
+        if k <= block:
+            window = data[k:k + block]
+            if find_outlier(point, window, std1):
                 data[k] = np.NaN
-        elif k >= len(data) - windowsize:
-            window = data[k - windowsize:k]
-            if find_outlier(point, window, n):
+        elif k >= len(data) - block:
+            window = data[k - block:k]
+            if find_outlier(point, window, std1):
                 data[k] = np.NaN
         else:
-            window = data[k - windowsize:k + windowsize]
-            if find_outlier(point, window, n):
+            window = data[k - block:k + block]
+            if find_outlier(point, window, std1):
                 data[k] = np.NaN
 
+    # Second run with std2:
+    for k, point in enumerate(data):
+        if k <= block:
+            window = data[k:k + block]
+            if find_outlier(point, window, std2):
+                data[k] = np.NaN
+        elif k >= len(data) - block:
+            window = data[k - block:k]
+            if find_outlier(point, window, std2):
+                data[k] = np.NaN
+        else:
+            window = data[k - block:k + block]
+            if find_outlier(point, window, std2):
+                data[k] = np.NaN
     return Series(data, index=self.index, name=self.name)
 
 
@@ -385,7 +451,7 @@ def from_fsi(cls, fname, verbose=True):
 
 
 @classmethod
-def from_cnv(cls, fname, verbose=True, cast_type='down'):
+def from_cnv(cls, fname, verbose=True):
     r"""Read ASCII CTD file from Seabird model cnv file.
     Return two DataFrames with up/down casts."""
     header, config, names = [], [], []
@@ -419,18 +485,6 @@ def from_cnv(cls, fname, verbose=True, cast_type='down'):
     cast = cls.from_records(recarray, index='prdm',
                                     exclude=None, coerce_float=True)
     cast.index.name = 'Pressure [db]'
-
-    # Get profile under 5 meters (after the CTD stabilization).
-    cast = cast[cast.index >= 5]
-
-    # Separate casts.
-    if cast_type == 'down':
-        cast = cast.ix[:cast.index.argmax()]
-    elif cast_type == 'up':
-        cast = cast.ix[cast.index.argmax():]
-    else:
-        print("Cast type %s not understood" % cast_type)
-        cast = None
 
     try:
         cast.name = basename(fname)[0]
@@ -544,6 +598,16 @@ def plot(self, **kwds):
     return fig, ax
 
 
+def swap_index(self, keys):
+    r"""Swap index with a columns in the DataFrame and return the index as a
+    new column."""
+    data = self.copy()
+    if not data.index.name:
+        data.index.name = "index"
+    data[data.index.name] = data.index.values
+    return data.set_index(keys, drop=True, append=False, verify_integrity=True)
+
+
 def plot_vars(self, variables=None, **kwds):
     r"""Plot CTD temperature and salinity."""
 
@@ -565,8 +629,10 @@ def plot_vars(self, variables=None, **kwds):
     ax1.set_xlabel("Salinity [kg g$^{-1}$]")
     ax1.invert_yaxis()
 
-    if cast.name:
+    try:
         fig.suptitle(r"Station %s profile" % cast.name)
+    except AttributeError:
+        print("Cast has no name.")
 
     ax0.legend(shadow=True, fancybox=True,
                numpoints=1, loc='lower right')
@@ -581,21 +647,26 @@ def plot_vars(self, variables=None, **kwds):
 
     return fig, (ax0, ax1)
 
+Index.asof = asof
+Index.float_ = float_
 
 Series.plot = plot
+Series.split = split
 Series.smooth = smooth
 Series.despike = despike
 Series.bindata = bindata
+Series.press_check = press_check
 
+DataFrame.split = split
 DataFrame.from_cnv = from_cnv
 DataFrame.from_edf = from_edf
 DataFrame.from_fsi = from_fsi
 DataFrame.plot_vars = plot_vars
+DataFrame.swap_index = swap_index
+DataFrame.press_check = press_check
 DataFrame.get_maxdepth = get_maxdepth
 DataFrame.plot_section = plot_section
-
-Index.asof = asof
-Index.float_ = float_
+DataFrame.seabird_filter = seabird_filter
 
 if __name__ == '__main__':
     import re
@@ -614,22 +685,31 @@ if __name__ == '__main__':
         cast['temperature'].plot()
     if 0:  # Seabird.
         fname = '/home/filipe/Dropbox/Work/LaDO/Projetos/AMBES/AMB09_data'
-        fname += '/DATA_SJ/CTD/AMB09_059_CTD_rad3.cnv'
+        #fname += '/DATA_SJ/CTD/AMB09_059_CTD_rad3.cnv'
+        fname += '/DATA_SJ/CTD/AMB09_004_CTD_rad1.cnv'
         cast = DataFrame.from_cnv(fname, verbose=False)
-        fig, ax = cast.plot_vars(['t090c', 'sal00'])
+        cast = cast[cast.index >= 5]
+        # Compute salinity.
+        cast['sal00'] = SP_from_C(cast['c0ms/cm'], cast['t090c'])
+        # Separate casts.
+        down = cast.split()
+        up = cast.split(up=True)
+        # Get profile under 5 meters (after the CTD stabilization).
+        #fig, ax = down.t090c.plot()
+        #ax.plot(up.t090c, up.index, 'r')
+        #fig, ax = down.plot_vars(['t090c', 'sal00'])
         if 0:  # Remove spikes.
             # Using the apply method.
-            kw = dict(n=3, windowsize=5, verbose=True)
-            despiked = cast.t090c.despike(**kw)
-            fig, ax = plt.subplots()
-            ax.plot(cast.t090c, cast.index, 'r', label='Original')
-            ax.plot(despiked, despiked.index, 'k.', alpha=0.5,
-                    label='de-spiked')
+            kw = dict(std1=20, std2=2, block=100)
+            despiked = down.sal00.despike(**kw)
+            mask = despiked.isnull()
+            fig, ax = down.sal00.plot(color='k', label='Original')
+            ax.plot(down.sal00[mask], down.sal00.index[mask], 'r.', alpha=0.5,
+                    label='Spikes removed with Wild Edit')
             ax.set_title("De-spiked vs Original data")
             ax.set_ylabel("Pressure [db]")
-            ax.set_xlabel("Temperature [%sC]" % degree)
+            ax.set_xlabel(r"Salinity [kg g^{-1}]")
             ax.legend(shadow=True, fancybox=True, numpoints=1, loc='best')
-            ax.invert_yaxis()
             offset = 0.05
             x1, x2 = ax.get_xlim()[0] - offset, ax.get_xlim()[1] + offset
             ax.set_xlim(x1, x2)
@@ -697,7 +777,7 @@ if __name__ == '__main__':
         # FIXME: Need to find a way to serial the metadata!
         np.savez("radial_01.npz", lon=lon, lat=lat)
 
-    if 1:  # Plot sections.
+    if 0:  # Plot sections.
         store = HDFStore("radial_01.h5", 'r')
         Temp = store['temperature']
         store.close()
@@ -707,3 +787,52 @@ if __name__ == '__main__':
 
         fig, ax, cb = Temp.plot_section()
         fig, ax, cb = Temp.plot_section(filled=True)
+
+    if 1:  # Loop Edit test.
+        fname = '/home/filipe/Dropbox/Work/LaDO/Projetos/AMBES/AMB09_data'
+        fname += '/DATA_SJ/CTD/AMB09_004_CTD_rad1.cnv'
+
+        cast = DataFrame.from_cnv(fname, verbose=False)
+        cast['sal'] = SP_from_C(cast['c0ms/cm'], cast['t090c'])
+        # Smooth velocity.
+        cast['dz/dtm'] = movingaverage(cast['dz/dtm'], window_size=48)
+
+        downcast, upcast = cast.split()
+
+        # Filter pressure.
+        kw = dict(sample_rate=24.0, time_constat=0.15)
+        downcast = downcast.seabird_filter(**kw)
+
+        # Threshold velocity from SeaBird manual.
+        mask_v = downcast['dz/dtm'] < 0.25
+
+        # Check inversion.
+        checked = downcast.press_check()
+        mask_p = checked['t090c'].isnull()
+
+        mask = ~mask_v * ~mask_p
+
+        fig, (ax0, ax1, ax2) = plt.subplots(ncols=3, sharex=True, sharey=True)
+        kw = dict(markersize=8., alpha=0.65)
+        ax0.plot(downcast['t090c'], downcast['Pressure [db]'], 'k.-', zorder=1,
+                label='Original', **kw)  # Original.
+        ax1.plot(downcast['t090c'], downcast['t090c'].index, 'k.-', zorder=1,
+                label='Filtered', **kw)  # Filtered + masks.
+
+        kw = dict(marker='o', linestyle='none', markerfacecolor='none',
+                markeredgecolor='g', zorder=3, markersize=7.)
+        ax1.plot(downcast['t090c'][mask_v], downcast.index[mask_v], **kw)
+
+        kw = dict(marker='o', linestyle='none', markerfacecolor='r',
+                markeredgecolor='none', zorder=2, markersize=6., alpha=0.65)
+        ax1.plot(downcast['t090c'][mask_p], downcast.index[mask_p], **kw)
+        ax2.plot(downcast['t090c'][mask], downcast.index[mask], 'k.-',
+                label='Keep', markersize=8., alpha=0.65)  # Final.
+        ax0.invert_yaxis()
+        ax0.grid(True)
+        ax1.grid(True)
+        ax2.grid(True)
+        ax0.legend(numpoints=1)
+        ax1.legend(numpoints=1)
+        ax2.legend(numpoints=1)
+        plt.show()
