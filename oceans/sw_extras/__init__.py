1e84c134239a28314c688387abd4280a3fb39aab
Filipe Fernandes <ocefpaf@gmail.com>
2020-01-27 17:49:39 -0300

    add init

---
+++ oceans/sw_extras/__init__.py
@@ -0,0 +1,25 @@
+from oceans.sw_extras.sw_extras import (
+    sigma_t,
+    sigmatheta,
+    N,
+    cph,
+    shear,
+    richnumb,
+    cor_beta,
+    inertial_period,
+    strat_period,
+    visc,
+    tcond,
+    spice,
+    psu2ppt,
+    soundspeed,
+    photic_depth,
+    cr_depth,
+    kdpar,
+    zmld_so,
+    zmld_boyer,
+    o2sol_SP_pt_benson_krause_84,
+    )
+
+from oceans.sw_extras.gamma_GP_from_SP_pt import gamma_GP_from_SP_pt
+from oceans.sw_extras.waves import Waves
