54daec94a21556b0e8af2935ca4661a5b5f09b0c
Kristen Thyng <kthyng@gmail.com>
2023-08-28 13:19:21 -0500

    combined two checks

--- oceans/filters.py
+++ oceans/filters.py
@@ -608,10 +608,8 @@ def pl33tn(x, dt=1.0, T=33.0, mode="valid", t=None):
     import pandas as pd
     import xarray as xr
 
-    if isinstance(x, xr.Dataset):
-        raise TypeError("Input a DataArray not a Dataset.")
-    elif isinstance(x, pd.DataFrame):
-        raise TypeError("Input a Series not a DataFrame.")
+    if isinstance(x, (xr.Dataset,pd.DataFrame)):
+        raise TypeError("Input a DataArray not a Dataset, or a Series not a DataFrame.")
 
     if isinstance(x, pd.Series) and not isinstance(
         x.index,
